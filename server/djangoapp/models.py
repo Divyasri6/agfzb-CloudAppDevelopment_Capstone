611ea7bfd1f7dcc501ce462043940220f5c97857
divyasri6 <divyasrivemula441@gmail.com>
2024-02-08 00:01:50 -0500

    Build CarModel and CarMake Django Models

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,24 +1,45 @@
 from django.db import models
 from django.utils.timezone import now
-
+from django.core import serializers
+import uuid
+import json
 
 # Create your models here.
+class CarMake(models.Model):
+    name = models.CharField(null=False, max_length=100, default='Make')
+    description = models.CharField(max_length=500)
 
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
+    def __str__(self):
+        return "Name: " + self.name
+
+class CarModel(models.Model):
+    id = models.IntegerField(default=1,primary_key=True)
+    name = models.CharField(null=False, max_length=100, default='Car')
+   
+    SEDAN = 'Sedan'
+    SUV = 'SUV'
+    WAGON = 'Wagon'
+    MINIVAN = 'Minivan'
+    CAR_TYPES = [
+        (SEDAN, 'Sedan'),
+        (SUV, 'SUV'),
+        (WAGON, 'Wagon'),
+        (MINIVAN, 'Minivan')
+    ]
+
+    type = models.CharField(
+        null=False,
+        max_length=50,
+        choices=CAR_TYPES,
+        default=SEDAN
+    )
+    make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    year = models.DateField(default=now)
+
+    def __str__(self):
+        return "Name: " + self.name  
 
 
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
-# - Name
-# - Dealer id, used to refer a dealer created in cloudant database
-# - Type (CharField with a choices argument to provide limited choices such as Sedan, SUV, WAGON, etc.)
-# - Year (DateField)
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
 
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
